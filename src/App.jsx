d96ea7ee72b4c3dfe7d288e7c225f2c93eb569f9
ShivaShankar401 <sivasankarst.anns@gmail.com>
2026-05-31 15:16:11 +0530

    Fixed Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function AppContent() {
   }
 
   return (
-    <Router basename='/inventory_management'>
+    <Router basename="/">
       <div className="app">
         {user && <Navbar />}
         <div className={user ? "main-content" : "full-content"}>
